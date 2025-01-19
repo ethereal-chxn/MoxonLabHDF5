a8d410cbaa5d4b3603492fd312bb6edd603fd2ab
ethereal-chxn <ethchan@ucdavis.edu>
2025-01-19 15:09:58 -0800

    Updated various functions and created some new ones to implement Dr. Moxon's feedback and implement spike storage
    - Rewrote store_neural to store both raw continuous data and spikes
    - Created new function, store_recording_session, which stores a raw data file into its own HDF5 file. Next steps include implementing metadata storage.
    - Refactored gen_dataset_name_from_filepath into gen_hdf5_name_from_filepath to incorporate Moxon's suggestion that each HDF5 file contains one recording session.

--- src/mox_h5.py
+++ src/mox_h5.py
@@ -7,12 +7,12 @@ import sys
 # || HELPER FUNCTIONS ||
 # ======================
 
-# Generate a dataset name from a given raw data file's path regardless of if given
+# Generate a HDF5 file name from a given raw data file's path regardless of if given
 # absolute or relative path.
-def gen_dataset_name_from_filepath(data_fp):
+def gen_hdf5_name_from_filepath(data_fp):
     data_fp_dirs = data_fp.split('/')
     file_name = data_fp_dirs[len(data_fp_dirs) - 1]
-    return file_name.split('.')[0]
+    return file_name.split('.')[0] + ".hdf5"
 
 
 # Attempts to access a specified subgroup. If subgroup doesn't exist,
@@ -36,16 +36,19 @@ def get_dataset(hdf5_obj, dataset_path):
         return
 
 
-# Stores analog signals within the raw EEG data file's NEO object form into
-# a specified subgroup in the specified HDF5 file object.
+# Stores raw continuous data within the raw EEG data file's NEO object form into
+# the specified HDF5 file object's Neural group.
 # ===
 # Note: Consult the Block, Segment, and AnalogSignal sections in 
 # https://neo.readthedocs.io/en/latest/api_reference.html# to understand
 # how EEG data is structured in an NEO object.
-def store_analog_signals(raw_data, hdf5_obj, subgroup_path, dataset_name):
+def store_raw_cont(raw_data, hdf5_obj):
+    print("== STORING RAW CONTINUOUS DATA ==")
     block_idx = 0
     segment_idx = 0
     signal_idx = 0
+
+    neural_group = get_subgroup(hdf5_obj, "Neural")
 
     for block in raw_data:
         segment_idx = 0
@@ -53,75 +56,79 @@ def store_analog_signals(raw_data, hdf5_obj, subgroup_path, dataset_name):
         for segment in block.segments:
             signal_idx = 0
             print(f"num of AnalogSignals in block {block_idx}, segment {segment_idx}: {len(segment.analogsignals)}")
-            curr_segment_signals_path = f"{subgroup_path}/{dataset_name}/Block{block_idx}/Segment{segment_idx}/AnalogSignals"
-            curr_segment_signals = get_subgroup(hdf5_obj, curr_segment_signals_path)
             for signal in segment.analogsignals:
                 print(f"shape of AnalogSignal {signal_idx} in block {block_idx}, segment {segment_idx}: {signal.shape}")
                 signal_np_arr = signal.magnitude
-                curr_segment_signals[f"AnalogSignal{signal_idx}"] = signal_np_arr
+                neural_group[f"RawContinuous{signal_idx}"] = signal_np_arr
                 signal_idx += 1
             segment_idx += 1
         block_idx += 1    
+
+# TODO: implement storing spike data into HDF5 file
+def store_spikes(raw_data, hdf5_obj):
+    print("== STORING SPIKES ==")
+    block_idx = 0
+    segment_idx = 0
+    spiketrain_idx = 0
+
+    neural_group = get_subgroup(hdf5_obj, "Neural")
+
+    for block in raw_data:
+        segment_idx = 0
+        print(f"num of segments in block {block_idx}: {len(block.segments)}")
+        for segment in block.segments:
+            spiketrain_idx = 0
+            print(f"num of SpikeTrains in block {block_idx}, segment {segment_idx}: {len(segment.spiketrains)}")
+            for signal in segment.spiketrains:
+                print(f"shape of SpikeTrains {spiketrain_idx} in block {block_idx}, segment {segment_idx}: {signal.shape}")
+                signal_np_arr = signal.magnitude
+                neural_group[f"Spikes{spiketrain_idx}"] = signal_np_arr
+                spiketrain_idx += 1
+            segment_idx += 1
+        block_idx += 1
 
 
 # ====================
 # || MOX_H5 METHODS ||
 # ====================
 
-# Stores a raw EEG data file into the HDF5 file's Neural group.
+# Stores a raw EEG file's data into the Neural group.
 # ===
 # Command format: store_neural [data_fp] [hdf5_fp] [subgroup]
 
-# TODO: get store_neural to work with the DRP003 txt files
-def store_neural(params):
-    # extract data_fp, hdf5_fp, and subgroup from user input
-    data_fp = params[0]
-    hdf5_fp = params[1]
-    subgroup = params[2]
+def store_neural(data_fp, hdf5_fp):
+    # extract data_fp and hdf5_fp from user input
+    # data_fp = params[0]
+    # hdf5_fp = params[1]
 
     # read in raw EEG file as a NEO object
     reader = neo.io.get_io(data_fp)
     raw_data = reader.read(lazy=False)
+
+    # FOR DEBUGGING PURPOSES
     print(f"num of blocks in raw data: {len(raw_data)}")
 
     # open/create hdf5 file at given path, hdf5_fp
     hdf5_obj = h5py.File(hdf5_fp, "a")
 
-    subgroup_path = "Neural/" + subgroup
-    dataset_name = gen_dataset_name_from_filepath(data_fp)
-
-    store_analog_signals(raw_data, hdf5_obj, subgroup_path, dataset_name)
-
-
-# Deletes a specified dataset from a specified HDF5 file's Neural group.
-# ===
-# Command format: delete_neural_ds [hdf5_obj] [dataset_path]
-def delete_neural_dataset(params):
-    hdf5_obj = params[0]
-    dataset_path = params[1]
-
-    # # Attempt to access dataset
-    # try:
-    #     specified_dataset = get_dataset(hdf5_obj, dataset_path)
-    # except:
-    #     print("Error in deleting neural dataset: Specified dataset does not exist.")
-    #     return
-    try:
-        del hdf5_obj[dataset_path]
-    except:
-        print("Error occurred in deleting neural dataset")
+    store_raw_cont(raw_data, hdf5_obj)
+    store_spikes(raw_data, hdf5_obj)
 
 
 # Stores a specified recording session data file into a HDF5 file.
 # ===
 # Command format: store_rs [data_path] [OPTIONAL: hdf5_file_name]
+
+# TODO: get store_recording_session to work with the DRP003 txt files
 def store_recording_session(params):
     data_path = params[0]
 
     if len(params) == 1:
-        hdf5_file_name = gen_dataset_name_from_filepath(data_path)
+        hdf5_file_name = gen_hdf5_name_from_filepath(data_path)
     else:
         hdf5_file_name = params[1]
+    
+    store_neural(data_path, hdf5_file_name)
 
 
 # TODO: refactor program to create HDF5 files for each recording session
@@ -135,11 +142,11 @@ if __name__ == '__main__':
     cmd_name = sys.argv[1]
     params = sys.argv[2:]
 
-    if cmd_name == "store_neural":
-        if len(params) != 3:
-            print("Usage: py ./src/mox_h5.py store_neural [data_fp] [hdf5_fp] [subgroup]")
+    if cmd_name == "store_rs":
+        if len(params) < 1:
+            print("Usage: py ./src/mox_h5.py store_rs [data_fp] [hdf5_fp]")
             exit(1)
-        store_neural(params)  
+        store_recording_session(params)
     else:
         print(f"Error: command {cmd_name} not found")
         exit(1)
